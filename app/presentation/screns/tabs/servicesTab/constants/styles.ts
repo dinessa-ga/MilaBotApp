d8a00cfae9712d06bb7cf9d1f3b7fb5669271a78
dinessa-ga <dinessa.ga@gmail.com>
2024-06-16 16:28:11 -0500

    last change

--- app/presentation/screns/tabs/servicesTab/constants/styles.ts
+++ app/presentation/screns/tabs/servicesTab/constants/styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     cardContainer: {
-        margin: 10,
+        margin: 5,
         
     },
     cardImage: {
