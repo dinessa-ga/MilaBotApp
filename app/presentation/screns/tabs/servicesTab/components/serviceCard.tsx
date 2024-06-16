5021f833ce91929adaf6111a35eee0c47b30e465
dinessa-ga <dinessa.ga@gmail.com>
2024-06-16 00:12:22 -0500

    feat: add logo midc, fix code

--- app/presentation/screns/tabs/servicesTab/components/serviceCard.tsx
+++ app/presentation/screns/tabs/servicesTab/components/serviceCard.tsx
@@ -4,27 +4,30 @@ import { styles } from '../constants/styles';
 import ServiceSchema from '../schemas/serviceSchema';
 
 
-const ServiceCardComponent = ({ color, image, name, description }: ServiceSchema) => {
+const ServiceCardComponent = (service) => {
     const theme = useTheme();
     const { background } = theme.colors;
 
     return (
-        
-        <Card style={[styles.cardContainer, { backgroundColor: theme.colors[color] }]}>
+        <Card style={[styles.cardContainer, { backgroundColor: theme.colors[service.color] }]}>
             <Card.Content style={styles.cardContent}>
-                <Card.Cover source={image}
+                <Card.Cover source={service.image}
                     style={styles.cardImage}
                 />
-                <View style={{ flex: 1 }} >
-                    <Image source={name} style={styles.cardLogo} />
-                    <Text variant="bodyLarge" style={{ color: background }}>
-                        {description}
+                <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', styles.id_cardTitle}} >
+                    <View variant="card_titles">
+                        <Text variant="bodyLarge" style={{ color: background }}>
+                            {service.name}
+                        </Text>
+                        <Image source={require('../assets/logo_midc_white.svg')} style={styles.cardLogo} />
+                    </View>
+                    <Text variant="large_description" style={{ color: background }}>
+                    {service.description}
                     </Text>
                 </View>
             </Card.Content>
         </Card>
-        
-    )
+    );
 };
 
 export default ServiceCardComponent;
